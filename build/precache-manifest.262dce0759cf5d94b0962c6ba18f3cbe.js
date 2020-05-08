d1be067982167c89a763af3551d2e627fb12c4fb
Eddie Madrigal <edmadrigal@yahoo.com>
2020-05-07 18:58:17 -0700

    app ready for deployment

---
+++ build/precache-manifest.262dce0759cf5d94b0962c6ba18f3cbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4cde8245332a6c83efc0cf2e19f64c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be0c0cccb89b42a5a822",
+    "url": "/static/css/main.fd69673b.chunk.css"
+  },
+  {
+    "revision": "4156587bafd7850a6689",
+    "url": "/static/js/2.e5efba66.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e5efba66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0c0cccb89b42a5a822",
+    "url": "/static/js/main.0a8b667e.chunk.js"
+  },
+  {
+    "revision": "c73f271e54cce2aa30ea",
+    "url": "/static/js/runtime-main.f9df417c.js"
+  }
+]);
